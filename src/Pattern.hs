f875b243c0fbff7365f507553490323182217a80
Radu Ometita <radu.ometita@gmail.com>
2018-07-23 12:59:05 +0300

    Initial implementation of data model and matrix operations.

---
+++ src/Pattern.hs
@@ -0,0 +1,126 @@
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pattern ( LHS
+               , PatternMatrix
+               , Metadata(..)
+               , Index
+               , Pattern(..)
+               , mkClauseMatrix
+               , DecisionTree(..)
+               ) where
+
+import           Data.Functor.Foldable (Fix (..), unfix)
+import           Data.Maybe            (mapMaybe)
+import           Data.Semigroup        ((<>))
+import           Data.Text             (Text (..), pack)
+import           TextShow              (showt)
+
+data Column a = Column
+                { getMetadata :: Metadata
+                , getTerms    :: [Pattern a]
+                } deriving (Show, Eq, Functor)
+
+newtype Metadata = Metadata [Metadata]
+                 deriving (Show, Eq)
+
+type Index       = Int
+data Pattern a   = Pattern Index [a]
+                 | Wildcard
+                 deriving (Show, Eq, Functor)
+
+newtype LHS a = LHS [Column a] deriving Functor
+
+type PatternMatrix = LHS (Fix Pattern)
+
+type Action       = Int
+data ClauseMatrix = ClauseMatrix (LHS (Fix Pattern)) [Action]
+
+-- [ Builders ]
+
+mkClauseMatrix :: [Column (Fix Pattern)]
+               -> [Action]
+               -> Either Text ClauseMatrix
+mkClauseMatrix cs as = do
+  validateColumnLength (length as) cs
+  pure (ClauseMatrix (LHS cs) as)
+  where
+    validateColumnLength :: Int -> [Column (Fix Pattern)] -> Either Text ()
+    validateColumnLength as =
+      mapM_ (\c ->
+                if length (getTerms c) == as
+                then Right ()
+                else Left $ "Wrong column length. Expected " <> showt as <>
+                            " and got " <> showt (length (getTerms c)))
+
+-- [ Matrix ]
+
+sigma :: Column a -> [Index]
+sigma = mapMaybe ix . getTerms
+  where
+    ix :: Pattern a -> Maybe Index
+    ix (Pattern ix _) = Just ix
+    ix Wildcard       = Nothing
+
+sigma₁ :: PatternMatrix -> [Index]
+sigma₁ (LHS (c : _)) = sigma c
+
+mSpecialize :: Index -> ClauseMatrix -> ClauseMatrix
+mSpecialize ix = expandMatrix ix . filterByIndex ix
+
+mDefault :: ClauseMatrix -> Maybe ClauseMatrix
+mDefault (ClauseMatrix (LHS (c : cs)) as) =
+  let (Metadata mtd) = getMetadata c
+      s₁ = sigma c
+  in  if length s₁ /= length mtd
+      then Just (ClauseMatrix (LHS cs) as)
+      else Nothing
+
+filterByList :: [Bool] -> [a] -> [a]
+filterByList (True  : bs) (x : xs) = x : filterByList bs xs
+filterByList (False : bs) (_ : xs) = filterByList bs xs
+filterByList _ _                   = []
+
+filterByIndex :: Index -> ClauseMatrix -> ClauseMatrix
+filterByIndex ix (ClauseMatrix (LHS (c : cs)) as) =
+  let filteredRows = map checkPatternIndex (getTerms c)
+      newCs = filterByList filteredRows cs
+      newAs = filterByList filteredRows as
+  in ClauseMatrix (LHS newCs) newAs
+  where
+    checkPatternIndex :: Pattern a -> Bool
+    checkPatternIndex Wildcard = True
+    checkPatternIndex (Pattern ix' _) = ix == ix'
+
+expandMatrix :: Index -> ClauseMatrix -> ClauseMatrix
+expandMatrix ix (ClauseMatrix (LHS (c : cs)) as) =
+  ClauseMatrix (LHS (expandColumn ix c <> cs)) as
+
+expandColumn :: Index -> Column (Fix Pattern) -> [Column (Fix Pattern)]
+expandColumn ix (Column m ps) =
+  let metas    = expandMetadata ix m
+      patterns = map (expandPattern metas) ps
+  in  zipWith Column metas patterns
+
+expandMetadata :: Index -> Metadata -> [Metadata]
+expandMetadata ix (Metadata ms) =
+  let (Metadata ms') = ms !! ix
+  in  ms'
+
+expandPattern :: [Metadata]
+              -> Pattern (Fix Pattern)
+              -> [Pattern (Fix Pattern)]
+expandPattern _ (Pattern ix fixedPs) = map unfix fixedPs
+expandPattern ms Wildcard            = replicate (length ms) Wildcard
+
+--[ Target language ]
+
+data L = L
+         { getSpecializations :: [(Index, Fix DecisionTree)]
+         , getDefault         :: Maybe (Fix DecisionTree)
+         }
+
+data DecisionTree a = Leaf Action
+                    | Fail
+                    | Switch L
+                    | Swap Index (Fix DecisionTree)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,44 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+
+import Data.Functor.Foldable (Fix(..))
+import Data.Proxy (Proxy(..))
+
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+
+
+import Pattern
+import Pattern.Class
+
+data Lst  = Cns Lst -- index 2
+          | Nil     -- index 1
+          | CnsW    -- index 0
+          | Wld     -- wildcard
+          deriving (Show, Eq)
+
+instance IsPattern Lst where
+  toPattern :: Lst -> Fix Pattern
+  toPattern Wld     = Fix Wildcard
+  toPattern CnsW    = Fix (Pattern 0 [Fix Wildcard])
+  toPattern Nil     = Fix (Pattern 1 [])
+  toPattern (Cns l) = Fix (Pattern 2 [toPattern l])
+
+instance HasMetadata Lst where
+  getMetadata :: Proxy Lst -> Metadata
+  getMetadata _ = Metadata
+                  [ Metadata []
+                  , Metadata []
+                  , Metadata [getMetadata (Proxy :: Proxy Lst)]
+                  ]
+
+tests :: TestTree
+tests = testGroup "Tests" [appendTests]
+
+appendTests :: TestTree
+appendTests = testGroup "Append tests"
+  [ testCase "append with one leg up" $
+      True @?= True ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
